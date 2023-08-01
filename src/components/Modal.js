7a7fcb05c6616290756387fee405205868011e4b
Berke Erdem Özdemir <berdemozdemir@gmail.com>
2023-08-02 00:47:09 +0300

    modal fixed

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,23 +1,24 @@
 function Modal({ onHideModal, children }) {
   return (
-    <>
-      <div
-        className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center"
-        onClick={onHideModal}
-      ></div>
-
+    <div className="fixed left-0 top-0 h-screen w-full flex justify-center items-center z-20">
       <dialog
         open
-        className="z-10 absolute top-[150px]  w-[520px] h-[400px] bg-white rounded-md flex flex-col "
+        className="z-10 w-[520px] h-[400px] bg-white rounded-md flex flex-col "
       >
         <header className="border-b-2 flex justify-between px-[10%] py-2 mb-8 items-center text-center">
           <h2 className="border-b-red-800 font-bold opacity-70">Basic Modal</h2>
-          <p className="opacity-50 cursor-pointer" onClick={onHideModal}>X</p>
+          <p className="opacity-50 cursor-pointer" onClick={onHideModal}>
+            X
+          </p>
         </header>
 
         {children}
       </dialog>
-    </>
+      <div
+        onClick={onHideModal}
+        className="z-[9]  w-full h-full bg-opacity-60 bg-black"
+      ></div>
+    </div>
   );
 }
 
